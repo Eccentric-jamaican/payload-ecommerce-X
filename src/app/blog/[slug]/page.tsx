c58661b9ede753ead95aa96cd4c1ce8fdf5dd780
Your Name <you@example.com>
2025-11-01 00:26:36 -0500

    feat: add /blog alias routes to avoid route-group bundling issues

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,1 @@
+export { default, generateMetadata, revalidate, runtime } from "../../(frontend)/(with_title)/blog/[slug]/page";
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,1 @@
+export { default, metadata, revalidate, runtime, dynamic } from "../(frontend)/(with_title)/blog/page";
